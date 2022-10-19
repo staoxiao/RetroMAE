77e94941e0a6c5aa04cb9c3202f1822f20ec8790
shitao <stxiao@bupt.edu.cn>
2022-10-20 00:41:55 +0800

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-# coding=utf-8
-# Copyright 2021 Reranker Author. All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 from setuptools import setup, find_packages
 
 setup(
